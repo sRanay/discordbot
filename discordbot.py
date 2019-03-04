64b656caedce1d6bd393eda92bfa14af0553884d
sRanay <jsong2506@gmail.com>
2019-03-04 20:59:05 +0800

    Added function for discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,14 +27,15 @@ async def change_status():
         await asyncio.sleep(5) # Change the status message based every 5 seconds
 
 # Speak something once in a while
-async def change_message():
+async def NormalMessage():
     await client.wait_until_ready()
+    channel = client.get_channel("456983229032038421")
 
     while not client.is_closed:
-        await client.say("FK LAHHHHHH")
-        await asyncio.sleep(3)
+        await client.send_message(channel, "FK LAHHHHHHHHHHHHHHH")
+        await asyncio.sleep(300)
 
-#Start of Commands
+# Start of Commands
 # clear command
 @client.command(pass_context=True)
 async def clear(ctx, amount=100):
@@ -47,7 +48,7 @@ async def clear(ctx, amount=100):
     await client.delete_messages(messages)
     await client.say("Messages Deleted")
 
-#Embed Function/Command
+# Embed Function/Command
 @client.command()
 async def displayembed():
     embed = discord.Embed(
@@ -69,6 +70,7 @@ async def displayembed():
 
     await client.say(embed=embed)
 
+# Help Function
 @client.command(pass_context=True)
 async def help(ctx):
     author = ctx.message.author
@@ -78,16 +80,30 @@ async def help(ctx):
     )
 
     embed.set_author(name='Help')
-    # List of Commands Help
+    # List of Commands using Help
     embed.add_field(name='clear', value='Clear the latest 100 messages less than 14 days ago', inline=False)
     embed.add_field(name='displayembed', value='Display the template for embed', inline=False)
 
     await client.send_message(author, embed=embed)
 
+# Join Function
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
 
+# Leave Function
+@client.command(pass_context=True)
+async def leave(ctx):
+    server = ctx.message.server
+    voice_client = client.voice_client_in(server)
+    await voice_client.disconnect()
 
-#Start of Events 
-#On member join
+# End of Commands
+
+# Start of Events 
+
+# On member join
 @client.event
 async def on_member_join(member):
     # Set the role to be normal
@@ -95,13 +111,25 @@ async def on_member_join(member):
     # Implement it for the current member that is joining
     await client.add_roles(member,role)
 
+# Reaction
+@client.event
+async def on_reaction_add(reaction,user):
+    channel = reaction.message.channel
+    await client.send_message(channel, '{} has added {} to the messasge: {}'.format(user.name, reaction.emoji, reaction.message.content))
+
+@client.event
+async def on_reaction_remove(reaction,user):
+    channel = reaction.message.channel
+    await client.send_message(channel, '{} has removed {} from the messasge: {}'.format(user.name, reaction.emoji, reaction.message.content))
+
+# End of Events
+
 # Starting up the discord bot
 @client.event
 async def on_ready():
     print('Bot is ready')
 
-
 # Running the bot
 client.loop.create_task(change_status())
-client.loop.create_task(change_message())
+client.loop.create_task(NormalMessage())
 client.run(TOKEN)
