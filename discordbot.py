555cfd6a0ca3d4a9f80fe8b5797391af064ebd37
Jason <33816512+sRanay@users.noreply.github.com>
2021-10-05 12:39:15 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 from discord.utils import get
 
 # Discord Bot Token
-TOKEN = "NDYzNTk4NzQ1NDkwMjkyNzM2.Dhz-ZA.wl-tqxvxSuIRFu1IMjKPxLmSktk"
+TOKEN = "<Discord Bot ID>"
 
 # Creating the client Object
 client = commands.Bot(command_prefix=".")
